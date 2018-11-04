b8749bc34acf89347450e4823fe03209bd9d08ee
Monkey <1466947023@qq.com>
2018-11-04 12:22:28 +0800

    尝试编写代码自动生成工具

---
+++ simple_code_generator/generator.py
@@ -0,0 +1,32 @@
+"""
+  Created by Xiaozhong on 2018/11/4.
+  Copyright (c) 2018/11/4 Xiaozhong. All rights reserved.
+"""
+
+from pathlib import Path
+
+
+def generator(dir_input):
+    """扫描指定目录下的*.java文件，并在当前目录下生成*_temp.java"""
+
+    current_dir = dir_input
+    count = 0
+    for pro_dir in current_dir.iterdir():
+        if pro_dir.is_file() and pro_dir.match("*.java") and not pro_dir.match("*_temp.java"):
+            s = str(dir_input) + "\\" + str(pro_dir.stem) + "_temp.java"
+            wr_file = open(s, mode="w", encoding="utf-8")
+            if wr_file.writable():
+                wr_file.write("/**\n * 自动生成的代码\n */")
+            count = count + 1
+        elif pro_dir.is_dir():
+            generator(pro_dir)
+    if dir_input.is_dir():
+        if count == 0:
+            print(str(dir_input) + "没有生成任何代码")
+        else:
+            print(str(dir_input) + "生成了" + str(count) + "个代码文件")
+
+
+c_dir = Path()
+
+generator(c_dir)
